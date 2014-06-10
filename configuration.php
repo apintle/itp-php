65a7b4d52c9759d44293987745c51c39967f75a2
Anamary <arpintle@gmail.com>
2014-06-10 14:13:46 -0500

    archivo con return (martes)

---
+++ configuration.php
@@ -0,0 +1,9 @@
+<?php
+return [
+        'dbname' => 'php_course',
+        'user' => 'root',
+        'password' => 'changeme',
+        'host' => 'localhost',
+];
+//aparentemente no tiene utilidad e return, pero si se usa desde otro archivo
+//si tene sentido al regresar una conección a red
